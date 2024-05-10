7c8ec66f5abeca1efb93ce73a125978e75b0bbd7
basilio-paulo <paulobasilio@nasajon.com.br>
2024-05-10 17:04:35 -0300

    a

--- src/main.py
+++ src/main.py
@@ -6,30 +6,16 @@ from routes.gedprod import gedprod
 
 
 app = FastAPI(
-    title="",#API_DATA['PROJECT_NAME'],
-    version="2024.04.0"#API_DATA['API_VERSION'],
+    title="SRE API",#API_DATA['PROJECT_NAME'],
+    description="API infraestrutura",#API_DATA['PROJECT_DESCRIPTION'],
+    version="2024.02.21"#API_DATA['API_VERSION'],
 )
-
-"""Configuracoes de CORS"""
-origins = [
-    "*",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
 
 app.include_router(gedprod.router)
 
 @app.get("/")
 def root():
     return {
-        "message": "For documentation, access /docs or /redoc",
-        "links": [f"{os.environ['DNS']}/docs", f"{os.environ['DNS']}/redoc"]
+        "message": "For documentation, access /docs, /redoc or /mkdocs",
+        "links": [f"{os.environ['DNS']}/docs", f"{os.environ['DNS']}/redoc", f"{os.environ['DNS']}/mkdocs"]
         }
